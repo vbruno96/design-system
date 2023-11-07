f8dce37bfe006cc0ddd8f30796fdb20e05ad48d9
Bruno Vinícius <vbruno96@gmail.com>
2023-11-06 22:08:40 -0300

    create another tokens

--- packages/tokens/src/index.ts
+++ packages/tokens/src/index.ts
@@ -1,1 +1,9 @@
 export * from './colors'
+
+export * from './font-sizes'
+export * from './font-weights'
+export * from './fonts'
+export * from './line-heights'
+
+export * from './radius'
+export * from './space'
