2f63da8bd19bc98a5b388c344d19e8d19ed28d2b
Bruno <vbruno96@gmail.com>
2023-11-09 11:20:40 -0300

    config stitches

---
+++ packages/react/src/styles/index.ts
@@ -0,0 +1,34 @@
+import {
+  colors,
+  fontSizes,
+  fontWeights,
+  fonts,
+  radius as radii,
+  space,
+} from '@ignite-ui/tokens'
+import { createStitches, defaultThemeMap } from '@stitches/react'
+
+export const {
+  styled,
+  css,
+  globalCss,
+  keyframes,
+  getCssText,
+  theme,
+  createTheme,
+  config,
+} = createStitches({
+  themeMap: {
+    ...defaultThemeMap,
+    height: 'space',
+    width: 'space',
+  },
+  theme: {
+    colors,
+    fonts,
+    fontSizes,
+    fontWeights,
+    radii,
+    space,
+  },
+})
