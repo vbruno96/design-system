d4c72f80787247768a38fc5e3a38c17350056e77
Bruno <vbruno96@gmail.com>
2023-11-14 14:29:41 -0300

    create feature for show tokens

---
+++ packages/docs/src/components/TokensGrid.tsx
@@ -0,0 +1,24 @@
+interface TokensGridProps {
+  tokens: Record<string, string>
+}
+
+export function TokensGrid({ tokens }: TokensGridProps) {
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Value</th>
+        </tr>
+      </thead>
+      <tbody>
+        {Object.entries(tokens).map(([key, value]) => (
+          <tr key={key}>
+            <td>{key}</td>
+            <td>{value}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  )
+}
